516845456e66232e1e87c4c1864d844f784f316a
elyobsemaj <54197717+elyobsemaj@users.noreply.github.com>
2020-02-15 22:00:52 -0800

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/centurion.jpg') {
+      myImage.setAttribute ('src','images/centurion2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/centurion.jpg');
+    }
+}
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'AMEX is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'AMEX is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'AMEX is cool, ' + myName;
+  }
+}
